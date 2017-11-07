75f3eb1e0d5d820d95c106bf10ee4e93acdab63e
Patrick Douel <32098830+Douel-P-BPC-Magenta@users.noreply.github.com>
2017-11-07 13:40:32 +0000

    Update Index.php

--- Index.php
+++ Index.php
@@ -21,6 +21,28 @@
 	echo "<br><br>";
 	echo "<br><br>";
 	echo "$fName";
-	?>
+	
+		
+	switch ($fName) {
+    case 1:
+        echo "You rolled a 1";
+        break;
+    case 2:
+        echo "You rolled a 2";
+        break;
+    case 3:
+        echo "You rolled a 3";
+        break;	
+    case 4:
+        echo "You rolled a 4";
+        break;
+    case 5:
+        echo "You rolled a 5";
+        break;
+    case 6:
+        echo "You rolled a 6";
+        break;	
+		
+		?>
 	</body>
 </html>
