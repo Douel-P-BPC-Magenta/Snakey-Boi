5dfd7bd33c2f689db12bf620ea71d0914cbfd58d
Patrick Douel <32098830+Douel-P-BPC-Magenta@users.noreply.github.com>
2017-11-07 13:27:05 +0000

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,24 @@
+	<html>
+	<head>
+	<title>Snakey Boi</title>
+	</head>
+	<body>
+	<?php
+	$fName=rand(1,6);
+	
+	
+	if ($Fname == "1") {
+	echo "Almost Snake Eyes";
+	}
+	elseif
+	($fullname == "6") {
+	echo "Winner";
+	}
+	else
+	{
+	echo "Get good noob";
+	}
+	
+	?>
+	</body>
+</html>
